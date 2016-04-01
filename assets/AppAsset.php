9061660d308ffe568e6487e9c0bd5f3bf4b610cd
zyq <zyq@CY-BD-FREEMAN.local>
2016-04-01 13:50:42 +0800

    init

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,6 +21,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'js/myJs.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
---
+++ views/demo/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <label><input type="radio" name='test' data-hide-id="#test" value=0>隐藏</label>
+    <label><input type="radio" name='test' data-show-id="#test" value=0>显示</label>
+    <div id='test' style="height:100px;width:100px;background:blue;"></div>
+</div>
